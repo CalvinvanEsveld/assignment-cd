82be13e7ee5822bd5ca3007b56fff876be90d073
Calvin <60155757+CalvinvanEsveld@users.noreply.github.com>
2023-04-25 15:40:59 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,6 @@
 import main
 
-def client():
-    with main.test_client() as client:
-        yield client
-
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert 'Hello, world!' in response.data
+    assert b'Hello, World!' in response.data
