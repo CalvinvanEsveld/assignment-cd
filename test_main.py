1ee9d16047df4d754e9ea0693c52cf772dfbd6d2
Calvin <60155757+CalvinvanEsveld@users.noreply.github.com>
2023-04-25 20:39:19 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,11 +1,3 @@
-import main
-import pytest
-
-@pytest.fixture
-def client():
-    with main.test_client() as client:
-        yield client
-
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
