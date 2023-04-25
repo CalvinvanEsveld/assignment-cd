b8731cf0a79d3a6047c43e5392bd5a9e64c0914a
Calvin <60155757+CalvinvanEsveld@users.noreply.github.com>
2023-04-25 15:09:58 +0200

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,11 @@
+import main
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert 'Hello, world!' in response.data
