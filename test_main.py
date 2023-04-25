e3225ed9839764c001e749a7a628f98109871451
Calvin <60155757+CalvinvanEsveld@users.noreply.github.com>
2023-04-25 21:24:13 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,20 +1,13 @@
-import main
+from main import app
+
 
 @pytest.fixture
-def test_client():
-    flask_app = create_app()
-    testing_client = flask_app.test_client()
- 
-    ctx = flask_app.app_context()
-    ctx.push()
- 
-    yield testing_client  # this line is the actual test client object
- 
-    ctx.pop()
+def client():
+    client = app.test_client()
+    return client
 
-
-def test_hello_world():
-    client = main.test_client()
-    response = client.get('/')
+def test_index(client):
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b'Hello, world!'
+    assert b"Hello, world!" in response.data
+
